9c6f406198b44ae9218b2bf3eecc0df875cba357
Brian Demers <bdemers@apache.org>
2022-02-28 21:27:00 -0500

    Update to 0.10.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
 packageName   = 'okta'
 fileType      = 'zip'
-url64bit      = 'https://github.com/okta/okta-cli/releases/download/okta-cli-tools-0.9.0/okta-cli-windows-0.9.0-x86_64.exe.zip'
+url64bit      = 'https://github.com/okta/okta-cli/releases/download/okta-cli-tools-0.10.0/okta-cli-windows-0.10.0-x86_64.exe.zip'
 destination   = $toolsDir
 
-checksum64    = '41f1799c675614b4ecfce11fdc055baa0a9b452e53c02695e30e4b55ead817a4'
+checksum64    = '9955bae08528837d7c821a05db680e30b0ef72a75d546692893ccc1bf1c780b3'
 checksumType64= 'sha256'
 }
 
